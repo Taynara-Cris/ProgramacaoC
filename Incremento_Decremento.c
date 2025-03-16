2ca5e151cabd150d40e3b8912bf10f40c78ba65f
Taynara-Cris <dev.taynarac@gmail.com>
2025-03-16 16:41:26 +0000

    Exercico3

---
+++ Incremento_Decremento.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+
+/*Incremento e Decremento: Aumenta ou diminui o valor de uma variável em uma unidade*/
+/*Incremento: (++)
+  Pré-incremento: ++a
+  Pós-incremento: a++
+  
+  Decremento: (--)
+  Pré-decremento: --a
+  Pós-decremento: a--*/
+
+int n1 = 10, resultado;
+
+printf("O valor antes do incremento é : %d\n", n1);
+
+resultado = n1++;
+//seria como se fosse n1 = n1 +1;
+//n1 += 1;
+//printf("O valor após o incremento é : %d\n", n1);
+printf("Após Pós-incremento- numero1: %d - resultado: %d\n", n1, resultado);
+
+n1--;
+//seria como se fosse n1 = n1 - 1;
+//n1 -= 1;
+printf("O valor após o decremento é : %d\n", n1);
+
+
+
+
+
+} 
+ 
+ 
