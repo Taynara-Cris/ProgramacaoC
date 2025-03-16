48dfec76c5c5746230fcdd0ce219e5699d5cbe9f
Taynara-Cris <dev.taynarac@gmail.com>
2025-03-16 16:22:05 +0000

    Finalizei o exercicio

--- Operadores_Atrib.c
+++ Operadores_Atrib.c
@@ -8,12 +8,28 @@ int main(){
       Atribuição com divisão (/=)
       */
 
-      int N1, N2, resultado;
+     
+
+      int N1 = 2, N2 = 12, resultado;
 
       resultado = 10;
       printf("Resultado é igual: %d\n", resultado);
-
-      resultado += 20;
-      printf("Resultado é igual: %d\n", resultado);
       
+      //resultado = resultado + 20;
+      resultado += 20;
+      printf("Resultado da + é igual: %d\n", resultado);
+      
+      //Resultado = resultado - N1;
+      resultado -= N1;
+      printf("Resultado da - é igual: %d\n", resultado);
+
+      //Resultado = resultado * 5;
+      resultado *= 5;
+      printf("Resultado da * é igual: %d\n", resultado);
+      
+      //Resultado = resultado / 2;
+      resultado /= 2;
+      printf("Resultado da / é igual: %d\n", resultado);
+
+
 }
