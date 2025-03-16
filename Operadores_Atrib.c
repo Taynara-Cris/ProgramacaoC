ed40f74e72352070e06f7be433362fcfbd654766
Taynara-Cris <dev.taynarac@gmail.com>
2025-03-16 16:07:40 +0000

    Segundo exercicio

---
+++ Operadores_Atrib.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+    /*Atribuição simples (=)
+      Atribuição com soma (+=)
+      Atribuição com subtração (-=)
+      Atribuição com multiplicação(*=)
+      Atribuição com divisão (/=)
+      */
+
+      int N1, N2, resultado;
+
+      resultado = 10;
+      printf("Resultado é igual: %d\n", resultado);
+
+      resultado += 20;
+      printf("Resultado é igual: %d\n", resultado);
+      
+}
