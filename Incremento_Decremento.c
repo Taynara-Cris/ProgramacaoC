2dc6accb571fd35d7d227240d26f3c2839fc0378
Taynara-Cris <dev.taynarac@gmail.com>
2025-03-16 16:46:55 +0000

    executando

--- Incremento_Decremento.c
+++ Incremento_Decremento.c
@@ -17,13 +17,22 @@ printf("O valor antes do incremento é : %d\n", n1);
 resultado = n1++;
 //seria como se fosse n1 = n1 +1;
 //n1 += 1;
+//Pós-incremento: 
+//resultado = n1;
+//n1++
 //printf("O valor após o incremento é : %d\n", n1);
 printf("Após Pós-incremento- numero1: %d - resultado: %d\n", n1, resultado);
+
+resultado = ++n1;
+
+printf("Após Pré-incremento- numero1: %d - resultado: %d\n", n1, resultado);
 
 n1--;
 //seria como se fosse n1 = n1 - 1;
 //n1 -= 1;
-printf("O valor após o decremento é : %d\n", n1);
+//printf("O valor após o decremento é : %d\n", n1);
+
+
 
 
 
