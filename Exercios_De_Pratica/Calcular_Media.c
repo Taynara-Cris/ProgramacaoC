595a1c816ce42cf9dcf2ff4f54fa53caef79fa18
Taynara-Cris <dev.taynarac@gmail.com>
2025-04-22 10:27:31 +0000

    Executando o exercicio do nivel intermediário

--- Exercios_De_Pratica/Calcular_Media.c
+++ Exercios_De_Pratica/Calcular_Media.c
@@ -1,1 +1,19 @@
-#includ
+#include<stdio.h>
+int main(){
+
+
+    float Nota1 = 5.2;
+    float Nota2 = 6.3;
+    float Média = Nota1 + Nota2 /2;
+
+    printf("Insira a primeira nota: ");
+    scanf("%.2f", &Nota1 );
+
+    printf("Insira a segunda nota: ");
+    scanf("%.2f", &Nota2);
+
+    printf("A sua média é: %.2f", Média);
+
+
+    return 0;
+}
