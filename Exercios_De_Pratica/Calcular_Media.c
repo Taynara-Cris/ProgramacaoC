023e614926d4cdb467181f8345d0cc237895d3de
Taynara-Cris <dev.taynarac@gmail.com>
2025-04-22 10:29:29 +0000

    Tentando fazer funcionar

--- Exercios_De_Pratica/Calcular_Media.c
+++ Exercios_De_Pratica/Calcular_Media.c
@@ -4,7 +4,7 @@ int main(){
 
     float Nota1 = 5.2;
     float Nota2 = 6.3;
-    float Média = Nota1 + Nota2 /2;
+    float Media = Nota1 + Nota2 /2;
 
     printf("Insira a primeira nota: ");
     scanf("%.2f", &Nota1 );
@@ -12,7 +12,7 @@ int main(){
     printf("Insira a segunda nota: ");
     scanf("%.2f", &Nota2);
 
-    printf("A sua média é: %.2f", Média);
+    printf("A sua média é: %.2f", Media);
 
 
     return 0;
